c8c28c8947efd66f027a8f8bbb133ca3d6f560f0
addyosmani <addyosmani@gmail.com>
2011-04-23 06:17:17 +0100

    adding refactor of next/prev

--- app/views/domready.js
+++ app/views/domready.js
@@ -5,34 +5,30 @@ $(function(){
 	
 	prevOption.hide();	
 	nextOption.hide();
-	
-	/*
-	all of this needs to be refactored - waaayyy too much rep. switch to e.prevDef as well.
-	*/
-	
-	nextOption.click(function(){
-		//workspace.search(workspace.q, parseInt(workspace.p)+1);
-		//todo: refactor all of this into a new function
-		var hashQuery = "", pageQuery = 0;	
-		(workspace.q == undefined) ? hashQuery = '' : hashQuery = workspace.q;
-		(workspace.p == undefined) ? pageQuery = 1  : pageQuery =  workspace.p;
-		(workspace.s == undefined) ? sortQuery = 'relevance' : sortQuery = workspace.s;
-		location.hash = 'search/' + hashQuery + '/s' + sortQuery + '/p' + (parseInt(pageQuery)+1);
-		return false;
+
+	nextOption.click(function(e){
+		e.preventDefault();
+		historySwitch('next');
 	});
 	
-	prevOption.click(function(){
-		var hashQuery = "", pageQuery = 0;	
-		(workspace.q == undefined) ? hashQuery = '' : hashQuery = workspace.q;
-		(workspace.p == undefined) ? pageQuery = 1  : pageQuery =  workspace.p;
-		(workspace.s == undefined) ? sortQuery = 'relevance' : sortQuery = workspace.s;
-		
-		pageQuery -=1;
-		if(pageQuery>=1){
-			location.hash = 'search/' + hashQuery + '/s' + sortQuery + '/p' + (parseInt(pageQuery));
-		}
-		return false;
+	prevOption.click(function(e){
+		e.preventDefault();
+		historySwitch('prev');
 	});
 	
 
 });
+
+
+
+function historySwitch(state){
+	var hashQuery = "", pageQuery = 0, increment = 0;
+	(workspace.q == undefined) ? hashQuery = '' : hashQuery = workspace.q;
+	(workspace.p == undefined) ? pageQuery = 1  : pageQuery =  workspace.p;
+	(workspace.s == undefined) ? sortQuery = 'relevance' : sortQuery = workspace.s;
+	
+	pageQuery = parseInt(pageQuery);
+	(state == 'next')? pageQuery +=1 : pageQuery -=1;
+	
+	(pageQuery <1)? null : location.hash = 'search/' + hashQuery + '/s' + sortQuery + '/p' + (pageQuery);
+}
